ae5ef1a6a096285260af871c4ae450b924dfd054
Renato Dehnhardt <renatotkd@gmail.com>
2021-02-25 17:57:54 +0100

    Remove everything

--- Ui/Web/Views/home.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('dashboard::layout')
-
-@section('title', __('dashboard::home.title'))
-
-@section('content')
-@endsection
--- Ui/Web/Views/layout.blade.php
+++
@@ -1,3 +0,0 @@
-@extends('ui::layouts.app')
-
-@section('title', __('dashboard::home.title'))
